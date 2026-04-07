a1ae2c4076cc5c5294c2c0f204ee735353150636
euanacode <luna.alyssaeuana@gmail.com>
2026-04-07 22:20:05 +0800

    Create python_calculator.py
    Name: Alyssa Euana Luna
    Section: CS3C

---
+++ python_calculator.py
@@ -0,0 +1,7 @@
+print("Select operation:")
+
+# Take input from the user
+choice = input("Enter choice (1/2/3/4): ")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
